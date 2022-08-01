7b9cce0add31ec442d405ba2743c8955dfb1f9ed
Klaudia Król <100020626+KlaudiaK0205@users.noreply.github.com>
2022-08-01 18:44:19 +0200

    Zadanie 1
    DONE

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,22 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+const newGalleryItem = galleryItems.map((galleryItem) => `<a class="gallery__item" href='${galleryItem.original}'>
+    <img 
+        class='gallery__image'
+        src='${galleryItem.preview}' 
+        alt='${galleryItem.description}' 
+        title = '${galleryItem.description}'/>
+    </a>`).join(' ');
+
+gallery.innerHTML = newGalleryItem;
+
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+// DONE
