999b3f7554dac1063192aab527144ef96d9b62dc
Klaudia Król <100020626+KlaudiaK0205@users.noreply.github.com>
2022-08-01 19:25:30 +0200

    Zadanie 3
    DONE

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,33 @@
-// import { throttle } from 'lodash';
+import { throttle } from 'lodash';
 
-// const form = document.querySelector("form.feedback-form");
-// const input = document.querySelector("input");
-// const textArea = document.querySelector("textarea");
+const form = document.querySelector("form.feedback-form");
+const input = document.querySelector("input");
+const textArea = document.querySelector("textarea");
 
-// form.addEventListener("input", throttle((event) => {
-//       const objectInput = { email: form.elements.email.value, message: form.elements.message.value };
-//       localStorage.setItem("feedback-form-state", JSON.stringify(objectInput));
-      
-// }, 500));
+form.addEventListener("input", throttle((event) => {
+    const input = { email: form.elements.email.value, message: form.elements.message.value };
+    localStorage.setItem("feedback-form-state", JSON.stringify(input));
+}, 500));
 
-// form.addEventListener("submit", (event) => {
-//       event.preventDefault();
-//       const { elements: { email, message } } = event.currentTarget;
-//       console.log({ email: email.value, message: message.value });
-      
-//       event.currentTarget.reset();
-//       localStorage.clear();
-// });
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const { elements: { email, message } } = event.currentTarget;
 
-// const storageData = localStorage.getItem("feedback-form-state");
-// const parseStorageData = JSON.parse(storageData);
-// const tryFoo = () => {
-//       if (parseStorageData !== null) {
-//             input.value = parseStorageData.email;
-//             textArea.value = parseStorageData.message;
-//       };
-// };
-// tryFoo();
+    console.log({ email: email.value, message: message.value });
+    
+    event.currentTarget.reset();
+    localStorage.clear();
+});
+
+const storage = localStorage.getItem("feedback-form-state");
+const parseStorageData = JSON.parse(storage);
+const tryFoo = () => {
+    if (parseStorageData !== null) {
+        input.value = parseStorageData.email;
+        textArea.value = parseStorageData.message;
+    };
+};
+
+tryFoo();
+
+// DONE
